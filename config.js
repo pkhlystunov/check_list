f7fe7721fad69f5608b85607af8c2fd4de69fbd5
PAVEL <108974875+pkhlystunov@users.noreply.github.com>
2026-09-17 08:52:53 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// Конфигурационный файл веб-приложения
+const CONFIG = {
+    // ⚠️ ВСТАВЬТЕ СЮДА ВАШ URL, ПОЛУЧЕННЫЙ ПОСЛЕ ДЕПЛОЯ ИЗ ЧАСТИ 1
+    API_URL: "https://script.google.com/macros/s/AKfycbwb12XNAhqk_M14aMCSA2sUM0Anp0sclVJG_Fo9UV02sWubX4UzH1_hhhi0QfgP5tyQ/exec"
+};
